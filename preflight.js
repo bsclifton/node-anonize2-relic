8ed12121145a4e12e7fdc0fee8768675e9b188a9
Brian Clifton <brian@clifton.me>
2016-06-18 23:52:30 -0700

    This will create the makefiles for Visual Studio and 3/25 projects compile. The others fail with error "Easy backend in 64-bit mode supported only in GCC compiler". Logs can be found in msvc14-error.log
    Two alternatives (not tested):
    -try to build using gcc under mingw
    -use mingw to generate library files for GMP, link against them. Involves specifying "-DARITH=gmp" (currently commented out; having trouble building gmp).
    
    If building with gcc works and the binary isn't dependent on mingw, the end product could possibly be packaged with this repo (instead of building from source).

--- preflight.js
+++ preflight.js
@@ -1,51 +1,78 @@
-var mkdir = require('fs').mkdir;
-var platform = require('os').platform();
-var spawn = require('child_process').spawn;
-  
+const mkdir = require('fs').mkdir;
+const platform = require('os').platform();
+const spawn = require('child_process').spawn;
+
 var configure = { darwin : [ '-DOPSYS=MACOS', '-DSTLIB=ON', '-DSHLIB=OFF', '-DALLOC=AUTO', '-DBENCH=0', '-DTESTS=0' ],
-                  linux  : [ '-DOPSYS=LINUX' ]
+                  linux  : [ '-DOPSYS=LINUX' ],
+                  win32  : [ '-DOPSYS=WINDOWS' ]//, '-DARITH=gmp' ]
                 }[platform];
+
 if (!configure) {
   console.log('unsupported platform: ' + platform);
   process.exit(1);
 }
 
-var args = [ '../relic', '-G', 'Unix Makefiles',
+const generator = platform === 'win32' ? 'Visual Studio 14 2015 Win64' : 'Unix Makefiles';
+const args = [ '../relic', '-G', generator,
              '-DCHECK=on', '-DDEBUG=on', '-DARCH=X64', '-DALIGN=16', '-DCOLOR=OFF', '-DSEED=UDEV',
              '-DWITH=BN;DV;FP;FPX;EP;EPX;PP;MD', '-DBN_PRECI=256', '-DBN_MAGNI=DOUBLE'
            ].concat(configure);
-
-
-var target = './anonize2/relic-build';
+const target = './anonize2/relic-build';
 
 mkdir(target, parseInt('755', 8), function (err) {
   var out;
-  var println = function (more) { out = Buffer.concat([ out, more ]); };
-
-  var loser = function (s, err) {
-    console.log(s);
-    console.log(err.toString());
-    console.log(out.toString());
-    process.exit(1);
-  };
 
   if ((err) && (err.code !== 'EEXIST')) throw err;
 
-  out = new Buffer(0);
-  var cmake = spawn('cmake', args, { cwd: target }).on('error', function (err) {
-    loser('cmake failed', err);
-  }).on('close', function (code) {
-    if (code) process.exit(code);
+  if (platform === 'win32') {
+    const execSync = require('child_process').execSync;
 
-    out = new Buffer(0);     
-    var make = spawn('make', [], { cwd: target }).on('error', function (err) {
-      loser('make failed', err);
-    }).on('close', process.exit);
+    // Create project files, etc needed to build relic
+    const cmakeCommand = 'cmake "' + args.join('" "') + '"';
+    console.log('Running CMake: (this should take ~5-10 seconds)\n' + cmakeCommand + '\n');
+    out = execSync(cmakeCommand, { cwd: target });
+    console.log('output:\n' + out.toString());
 
-    make.stdout.on('data', function (more) { console.log(more.toString().trim()); });
-    make.stderr.on('data', println);
-  });
+    // Build relic w/ Visual Studio
+    // TODO: this fails! :( only works w/ GCC
+    const buildArgs = [
+      'pushd .',
+      'cd "\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC"',
+      'vcvarsall x86',
+      'popd',
+      'devenv RELIC.sln /build'
+    ];
+    const buildCommand = buildArgs.join(' && ');
+    console.log('Building using Visual Studio 2015:\n' + buildCommand + '\n');
+    out = execSync(buildCommand, { cwd: target });
+    console.log('output:\n' + out.toString());
 
-  cmake.stdout.on('data', println);
-  cmake.stderr.on('data', println);
+  } else {
+    var println = function (more) { out = Buffer.concat([ out, more ]); };
+
+    var loser = function (s, err) {
+      console.log(s);
+      console.log(err.toString());
+      console.log(out.toString());
+      process.exit(1);
+    };
+
+    out = new Buffer(0);
+    var cmake = spawn('cmake', args, { cwd: target }).on('error', function (err) {
+      loser('cmake failed', err);
+    }).on('close', function (code) {
+      if (code) process.exit(code);
+
+      out = new Buffer(0);
+      var make = spawn('make', [], { cwd: target }).on('error', function (err) {
+        loser('make failed', err);
+      }).on('close', process.exit);
+
+      make.stdout.on('data', function (more) { console.log(more.toString().trim()); });
+      make.stderr.on('data', println);
+    });
+
+    cmake.stdout.on('data', println);
+    cmake.stderr.on('data', println);
+  }
 });
